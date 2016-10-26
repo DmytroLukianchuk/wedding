dc201e67422784ca5f409feea2d3ea7abef4f6a6
dmytrol <dmytrol@wix.com>
2016-10-26 13:04:18 +0300

    Introduced the constant of time between photos (TIME_BETWEEN_SLIDES).

--- wedding_keyboard.py
+++ wedding_keyboard.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 
+# set the time delay between slides
+TIME_BETWEEN_SLIDES = 5
 # navigation to the first image of the gallery
 main_url = "http://dimaplusnadia.wixsite.com/dimanadialovestory/fullscreen-page/comp-iui1lr44/f9697f2b-3439-4542-b4b7" \
            "-748bfbab1a1f/0/%3Fi%3D0%26p%3Dc1dmp%26s%3Dstyle-iui32ah4"
@@ -24,18 +26,18 @@ while True:
     # allocate expand and close buttons
     expand_button = driver.find_element_by_class_name('fullscreen-expand')
     close_button = driver.find_element_by_class_name('fullscreen-close')
-    # click expand and wait for three seconds
+    # click expand and wait
     expand_button.click()
 
-    time.sleep(3)
+    time.sleep(TIME_BETWEEN_SLIDES)
     # set the general quantity of the gallery
     image_quantity = 180
     # changing photos by clicking right arrow on the keyboard till the end of the gallery
     while image_quantity > 0:
         next_image_action = ActionChains(driver).send_keys('\ue014')
         next_image_action.perform()
-        # delay between slides is 3 sec
-        time.sleep(3)
+        # delay between slides is TIME_BETWEEN_SLIDES value sec
+        time.sleep(TIME_BETWEEN_SLIDES)
 
         image_quantity -= 1
     # close the expand mode when end of gallery
